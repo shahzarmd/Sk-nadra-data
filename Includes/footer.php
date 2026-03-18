7e6f5180236f3290fc876501853b6b9f5aa466b8
Shahzar khan <181156178+shahzarmd@users.noreply.github.com>
2026-03-18 06:03:27 +0500

    Add files via upload

---
+++ Includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="foot" style="position: fixed; bottom: 0; width: 100%; background-color: #333; color: white; text-align: center; padding: 10px;">
+    <p>&copy; 2024 NADRA. All rights reserved.</p>
+</footer>
+
+</body>
+</html>
